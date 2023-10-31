cdc544590b6407e298f7d662f09ce70e85ca1b61
sensizlik123 <celicavus13@gmail.com>
2023-10-31 15:56:39 +0300

    react ile random number

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import "./style.css";
+import React, { useState } from "react";
+
+function App() {
+  const [minVal, setMinVal] = useState(0);
+  const [maxVal, setMaxVal] = useState(10);
+  const [randomNum, setrandomNum] = useState(10);
+
+  const getRandomNumber = () => {
+    setrandomNum(Math.floor(Math.random() * (maxVal - minVal + 1) + minVal))
+  }
+  return (
+    <div className="hero">
+      <div className="container">
+        <div className="randomNum">
+          <p>Random Number : <span>{randomNum}</span></p>
+        </div>
+
+        <div className="numContainer">
+          <div>
+            <p>Min : </p>
+            <input type="number"
+              value={minVal}
+              onChange={e => setMinVal(+e.target.value)}
+            />
+          </div>
+
+          <div>
+            <p>Max : </p>
+            <input type="number"
+              value={maxVal}
+              onChange={e => setMaxVal(+e.target.value)}
+            />
+          </div>
+
+        </div>
+
+        <button onClick={getRandomNumber}>Get Random Number</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
